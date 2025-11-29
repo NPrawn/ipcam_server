4d33466b077008da768e111a10d7bf6323219516
jeong <ljwljw522@gmail.com>
2025-11-29 17:53:12 +0900

    fix: debug

--- debug.py
+++ debug.py
@@ -4,8 +4,8 @@ from app.utils.streaming_be_create import call_be_create_info
 if __name__ == "__main__":
     ok, data = call_be_create_info(
         device_id="dev-1234",
-        device_pubkey_b64u="여기에실제기기공개키(Base64URL)",
-        reg_jwt="여기에실제-등록JWT",
+        device_pubkey_b64u="qweqwe",
+        reg_jwt="jwtjwt",
         owner_user_id=1,
         timeout=5,
     )
